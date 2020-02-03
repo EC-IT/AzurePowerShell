0e30d9a9158e11da6f65d2f8eab1e0fb7ae4c34f
MPE <60610152+EC-IT@users.noreply.github.com>
2020-02-03 14:37:14 +0100

    Create Get-AzWebAppCredentials.ps1
    Gets all Azure Web App users, passwors and URL

---
+++ Get-AzWebAppCredentials.ps1
@@ -0,0 +1,29 @@
+﻿# MPE - 31 01 2020
+
+function Get-AzWebAppCredentials()
+{
+	# recupere les AppService
+	$appservice_list = Get-AzWebApp 
+
+	# Cree tableau des resultats
+	$PublishingProfile_list = @()
+
+	# Pour chaque appservice....
+	foreach ( $app in $appservice_list )
+	{
+		# ...recupere le nom, l'url et les ID
+		[xml]$pp = Get-AzWebAppPublishingProfile -ResourceGroupName $app.ResourceGroup -Name $app.Name
+		$appli = "" | Select user,pass,url,nom
+		$appli.user = $pp.publishData.publishProfile[0].userName
+		$appli.pass = $pp.publishData.publishProfile[0].userPWD
+		$appli.url = $pp.publishData.publishProfile[0].publishUrl
+		$appli.nom = $app.Name
+
+		# ajoute les info de l'appservice au tableau resultat
+		$PublishingProfile_list += $appli
+
+	}
+
+	# Affiche le résultat
+	$PublishingProfile_list
+}
