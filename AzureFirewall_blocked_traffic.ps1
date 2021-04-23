4427cc15fdd6ad47045aadc380e6e5d18e93eab3
EC-IT <60610152+EC-IT@users.noreply.github.com>
2021-04-23 18:05:27 +0200

    firewall logs scripts

---
+++ AzureFirewall_blocked_traffic.ps1
@@ -0,0 +1,35 @@
+﻿# MPE - MARS/AVRIL 2021
+
+function Get-AzWorkspaceID()
+{
+    $workspaces = Get-AzOperationalInsightsWorkspace
+    foreach ( $workpace in $workspaces)
+    {
+        if ( $workpace.Name -notlike "*Default*" -and $workpace.Name -notlike "*Test*"  -and $workpace.Name -notlike "*dev*" )
+        {
+            return $workpace.CustomerId.Guid;
+        }
+    }
+}
+$WorkspaceID = Get-AzWorkspaceID
+
+# Liste des IP connus dans les regles du parfeu
+$AzureFirewall01 = Get-AzFirewall
+$ip_trust = $AzureFirewall01.NetworkRuleCollections.Rules.DestinationAddresses | ?{$_ -notmatch '\*'} | ?{$_ -notmatch '/'} 
+
+# QUERY LOGS
+$q='AzureDiagnostics | where Category == "AzureFirewallNetworkRule" and msg_s contains "Deny" and TimeGenerated > ago(24h) | distinct  msg_s'
+$queryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId "$WorkspaceID" -Query $q
+
+# GET LOGS NOT ALLOW
+$result_not_allow = $( ($queryResults.Results) | ?{$_ -notmatch 'Allow'} )
+$result_not_allow_uniq = $( $result_not_allow | ForEach-Object{($_ -split "\s+")[5]} | Sort-Object -Unique )
+
+# liste des IP dans les logs
+$ip_unique = $result_not_allow_uniq | Foreach-Object{ $_.Split(':')[0] }  | Sort-Object -Unique
+
+# liste IP inconnu
+$ip_unknown = $ip_unique 
+
+echo "IP dans les LOGS, mais pas dans les regles de parfeu : "
+$ip_unknown 
---
+++ NSG_list_unused_rules.ps1
@@ -0,0 +1,38 @@
+﻿# MPE - MARS/AVRIL 2021
+
+function Get-AzWorkspaceID()
+{
+    $workspaces = Get-AzOperationalInsightsWorkspace
+    foreach ( $workpace in $workspaces)
+    {
+        if ( $workpace.Name -notlike "*Default*" -and $workpace.Name -notlike "*Test*"  -and $workpace.Name -notlike "*dev*" )
+        {
+            return $workpace.CustomerId.Guid;
+        }
+    }
+}
+$WorkspaceID = Get-AzWorkspaceID
+
+# List NSG
+$nsg_list = Get-AzNetworkSecurityGroup 
+
+# FOR EACH NSG...
+foreach ($nsg in $nsg_list)
+{
+    $nsg_name = $nsg.Name
+    echo "`n#### $nsg_name ####"
+
+    # QUERY LOGS
+    $q="AzureNetworkAnalytics_CL | where NSGList_s contains `"$nsg_name`" and TimeGenerated > ago(7d) | distinct NSGRule_s  "
+    $queryResults = Invoke-AzOperationalInsightsQuery -WorkspaceId "$WorkspaceID" -Query $q
+    $rules_in_logs = $( ($queryResults.Results) )
+
+    # ...CHECK IF RULES IN LOGS
+    foreach($rule in $nsg.SecurityRules)
+    {
+        if ( ! $($rules_in_logs | Select-String $rule.Name ) )   
+        {
+            $rule.Name
+        }
+    }
+}
