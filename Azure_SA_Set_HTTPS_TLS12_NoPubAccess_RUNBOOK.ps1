efa744f16a4defb9c3f5b22b89581c0d95310f11
EC-IT <60610152+EC-IT@users.noreply.github.com>
2022-01-17 11:42:32 +0100

    add policys

---
+++ Azure_SA_Set_HTTPS_TLS12_NoPubAccess_RUNBOOK.ps1
@@ -0,0 +1,35 @@
+﻿# MPE - JAN 2022
+# Storage Account Set TLS 1.2
+Import-Module Az.Storage
+
+#Connection
+$connectionName = "AzureRunAsConnection"
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName  
+Add-AzAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+
+function Set-SecureStorageAccount()
+{
+    $sas = Get-AzStorageAccount
+
+    foreach($sa in $sas)
+    {
+        if ( $sa.MinimumTlsVersion -ne "TLS1_2" )
+        {        
+                Set-AzStorageAccount -ResourceGroupName $sa.ResourceGroupName -Name $sa.StorageAccountName -MinimumTlsVersion TLS1_2
+        }
+       # if ( $sa.AllowBlobPublicAccess )
+       # {        
+       #         Set-AzStorageAccount -ResourceGroupName $sa.ResourceGroupName -Name $sa.StorageAccountName -AllowBlobPublicAccess $false
+       # }
+        if ( ! $sa.EnableHttpsTrafficOnly )
+        {        
+                Set-AzStorageAccount -ResourceGroupName $sa.ResourceGroupName -Name $sa.StorageAccountName -EnableHttpsTrafficOnly $true
+        }
+    }
+}
+
+if ($servicePrincipalConnection)
+{
+    Set-SecureStorageAccount
+}
